578e9f376e25fbf110b9b125548eac841a4555df
Jeff Dyer <jeffdyer@acm.org>
2018-08-13 12:57:06 -0700

    fix m\angle

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-let id = "9Wbid12qfd";  // Current best rule set
+let id = "o5XSVwe4Uj";  // Current best rule set
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
