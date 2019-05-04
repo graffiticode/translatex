3c16a6e28d160c9a99d8ec6ecad709917b14ea2c
Jeff Dyer <jeffdyer@acm.org>
2019-05-04 11:08:57 -0700

    Megan's May 1 rules

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-const id = "dOMcoXWlHa";  // Current best rule set
+const id = "0vgCLMoviA";  // Current best rule set
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
