9a49c59debae46682fefb3f010309778b9278e3e
Jeff Dyer <jeffdyer@acm.org>
2019-05-30 11:35:19 -0700

    Update rules

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-const id = "RQYT5oeQSr";  // Current best rule set
+const id = "Bq8fVzyzH9";  // Current best rule set
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
