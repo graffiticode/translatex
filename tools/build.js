8e7bef750a36438cded2ca588f15373e442b0c10
Megan Ly <megan@artcompiler.com>
2019-10-08 14:02:33 -0700

    don't use nsimplify in detecting rationals

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-const id = "QV8F6JqAphN";  // Current best rule set
+const id = "Wz8tbq3lgsp";  // Current best rule set
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
