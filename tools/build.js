0caa422874ecba0d3b142c5bbb58af332934ddbc
Megan Ly <megan@artcompiler.com>
2021-01-12 10:08:06 -0800

    Update latexToLatex rules for \infty

--- tools/build.js
+++ tools/build.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import {execSync} from "child_process";
 
-const id = "b1yt7Mer5tL";  // Current best rule set for latex-to-latex translation.
+const id = "AK8Iqnl3QFM";  // Current best rule set for latex-to-latex translation.
 
 function rmdir(path) {
   try { var files = fs.readdirSync(path); }
